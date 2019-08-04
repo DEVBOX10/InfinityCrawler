8c5a4a52fdb10d9daa0cca482e42493651b35cdb
Turnerj <git@turnerj.com>
2019-08-04 23:30:36 +0930

    Direct access to install location

--- custom-chocolatey.ps1
+++ custom-chocolatey.ps1
@@ -1,18 +1,1 @@
-#Get-Content -Path 'C:\ProgramData\chocolatey\logs\chocolatey.log'
-Exit 1
-
-# Chocolatey profile
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-  Write-Host "Trying import..."
-  Import-Module "$ChocolateyProfile"
-}
-
-refreshenv
-
-$packageName = 'opencover'
-$installerType = 'msi'
-$silentArgs= ''
-$url = 'https://github.com/OpenCover/opencover/releases/download/4.7.922/opencover.4.7.922.msi'
-$checkSum = "87EBF6C0172BE019238C615431F916B8905C17632EF55A9796980C777499F94A"
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes @(0) -ChecksumType "sha256" -Checksum $checkSum
+%LocalAppData%/Apps/OpenCover/OpenCover.Console.exe
